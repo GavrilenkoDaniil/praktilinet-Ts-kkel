e510b054abbecc35c34bbcd8e7ea033948c62873
opilane <opilane@HP-CZC6297B89>
2021-10-29 12:25:58 +0300

    Add project files.

---
+++ praktilinetöö_Tsükkel.py
@@ -0,0 +1,19 @@
+#Ülesanne 1
+print("tervist!")
+nimi=""
+nimi=str(input("Palun sisesta oma nimi:"))
+for i in range(1,6):
+    print(f"Ole tervitatud {nimi}, {i}. korda.")
+
+#Ülesanne 2
+n1 = int(input('Sisesta number 1: '))
+n2 = int(input('Sisesta number 2: '))
+n3 = int(input('Sisesta number 3: '))
+n4 = int(input('Sisesta number 4: '))
+n5 = int(input('Sisesta number 5: '))
+n6 = int(input('Sisesta number 6: '))
+n7 = int(input('Sisesta number 7: '))
+n8 = int(input('Sisesta number 8: '))
+n9 = int(input('Sisesta number 9: '))
+n10 = int(input('Sisesta number 10: '))
+print(n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)
